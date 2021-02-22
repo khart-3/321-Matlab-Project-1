cba10555d8fc55e74e76680c52d49994627e4c50
jproulx1 <47118186+jproulx1@users.noreply.github.com>
2021-02-22 12:37:49 -0500

    Add files via upload

--- 321 P1/project1.m
+++ 321 P1/project1.m
@@ -5,19 +5,34 @@
 %y=sin(2*pi*freq*T);
 
 
-eigth_b=gentone(246.94,0.22,1);
-half_g=gentone(392,0.97,1);
+eigth_g=gentone(392,0.22,1);
+half_e=gentone(311.13,0.97,1);
 eigth_f=gentone(349.23, 0.22,1);
-half_c=gentone(261.63,0.97,1);
+half_d=gentone(293.66,0.97,1);
 %eigth_rest=genrest(0.25);
 %rest=genrest(0.03);
 %soundsc(eigth_b);
-subplot(2,1,1);
-y=eigth_b;
-x=[0:1/1858:0.97];
+subplot(4,1,1);
+y=eigth_g;
+x=[0:1/8192:.22];
 plot(x,y)
-title('Eighth B')
-subplot(2,1,2);
+title('Eighth G')
+subplot(4,1,2);
 y2=eigth_f;
 plot(x,y2);
 title('Eighth F')
+subplot(4,1,3)
+y3=half_e;
+x2=[0:1/8192:0.97];
+plot(x2,y3)
+title('Half E')
+subplot(4,1,4)
+y4=half_d;
+x2=[0:1/8192:0.97];
+plot(x2,y4)
+title('Half D')
+
+melody=[eigth_g eigth_g eigth_g half_e eigth_f eigth_f eigth_f half_d]
+soundsc(melody);
+
+
