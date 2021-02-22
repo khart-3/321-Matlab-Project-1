f217ed6139ff3ad96e8b4042c96a25ec8b11f53c
jproulx1 <47118186+jproulx1@users.noreply.github.com>
2021-02-22 13:23:17 -0500

    Add files via upload

---
+++ 321 P1/genrest.m
@@ -0,0 +1,10 @@
+function [rest] = genrest(length)
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+T=[0:1/8192:length];
+rest=T;
+
+end
+
--- 321 P1/project1.m
+++ 321 P1/project1.m
@@ -5,13 +5,16 @@
 %y=sin(2*pi*freq*T);
 
 
+eigth_rest=genrest(0.25);
 eigth_g=gentone(392,0.22,1);
 half_e=gentone(311.13,0.97,1);
 eigth_f=gentone(349.23, 0.22,1);
 half_d=gentone(293.66,0.97,1);
+smallrest=genrest(0.03);
 %eigth_rest=genrest(0.25);
 %rest=genrest(0.03);
 %soundsc(eigth_b);
+figure(1)
 subplot(4,1,1);
 y=eigth_g;
 x=[0:1/8192:.22];
@@ -32,7 +35,9 @@ x2=[0:1/8192:0.97];
 plot(x2,y4)
 title('Half D')
 
-melody=[eigth_g eigth_g eigth_g half_e eigth_f eigth_f eigth_f half_d]
-soundsc(melody);
-
+melody=[eigth_rest smallrest eigth_g smallrest eigth_g smallrest eigth_g smallrest half_e smallrest eigth_rest eigth_f smallrest eigth_f smallrest eigth_f smallrest half_d]
+%soundsc(melody);
+figure(2)
+spectrogram(melody,256,196, 512 , 8192,'yaxis')
+%up to this point written by Joshua Proulx
 
