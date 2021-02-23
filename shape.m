db03a626ff505dece08ec949de9677f1d5a9dd1b
khart-3 <57810103+khart-3@users.noreply.github.com>
2021-02-22 19:49:52 -0500

    Create shape.m

---
+++ shape.m
@@ -0,0 +1,9 @@
+figure(1)
+y=[0:.1:1];
+x=[0:.05:.25];
+plot([0 .025 .05 .23 .25],[0 1 .6 .6 0])
+title('Amplitude Profile');
+xlabel('Time in sec');
+ylabel('Amplitude');
+
+%written by Kaitlyn Hart
